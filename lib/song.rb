c74e60d05e9fd8277a55c321fdb5d654a107a79e
LS-C <suzy.cho111@gmail.com>
2017-08-10 11:29:47 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+
+    @@count = 0
+    @@artists= []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@artists <<artist
+        @@genres <<genre
+        @@count +=1
+    end
+
+    def name
+        @name
+    end
+
+    def artist
+        @artist
+    end
+
+    def genre
+        @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        counts = Hash.new(0)
+        @@genres.each { |genre| counts[genre] +=1 }
+        counts
+    end
+
+    def self.artist_count
+        counts = Hash.new(0)
+        @@artists.each { |artist| counts[artist] +=1 }
+        counts
+    end
+
+end
+
+
